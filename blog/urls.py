001cb8ff53dd874289f72905e716681bd03507e0
soude <soude.amiri17@gmail.com>
2025-03-13 07:44:31 +0330

    add article detail page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path,re_path
+from .views import article_detail
 
 
+app_name='blog'
 urlpatterns = [
-    
+    re_path(r'detail/(?P<slug>[-\w]+)/',article_detail,name='article_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Article
 
-# Create your views here.
+
+def article_detail(request,slug):
+    article = get_object_or_404(Article,slug=slug)
+    viewed_articles = request.session.get('viewed_articles', [])
+    if article.id not in viewed_articles:
+        article.views += 1
+        article.save(update_fields=['views'])
+        viewed_articles.append(article.id)
+        request.session['viewed_articles'] = viewed_articles
+    context ={
+        'article':article
+    }
+    return render(request,'blog/article_detail.html',context)
+    
