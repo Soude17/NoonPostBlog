2d4a5aaa5dd30a560389104ef707e40c1d9b6775
soude <soude.amiri17@gmail.com>
2025-03-16 08:53:52 +0330

    add category_article and article_list page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,re_path
-from .views import article_detail
+from .views import article_detail,article_list,category_article
 
 
 app_name='blog'
 urlpatterns = [
     re_path(r'detail/(?P<slug>[-\w]+)/',article_detail,name='article_detail'),
+    path('article_list/',article_list,name='article_list'),
+    re_path(r'category/(?P<slug>[-\w]+)/',category_article,name='category_article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Article
+from .models import Article,Category
 
 
 def article_detail(request,slug):
+    latest_articles = Article.objects.filter(status=True).order_by('-created')[:4]
+    categories = Category.objects.all()
     article = get_object_or_404(Article,slug=slug)
     viewed_articles = request.session.get('viewed_articles', [])
     if article.id not in viewed_articles:
@@ -11,7 +13,34 @@ def article_detail(request,slug):
         viewed_articles.append(article.id)
         request.session['viewed_articles'] = viewed_articles
     context ={
-        'article':article
+        'article':article,
+        'latest_articles':latest_articles,
+        'categories':categories,
     }
     return render(request,'blog/article_detail.html',context)
+
+
+def article_list(request):
+    latest_articles = Article.objects.filter(status=True).order_by('-created')[:4]
+    categories = Category.objects.all()
+    articles = Article.objects.filter(status=True)
+    context ={
+        'articles':articles,
+        'latest_articles':latest_articles,
+        'categories':categories,
+    }
+    return render(request,'blog/article_list.html',context)    
+
+def category_article(request,slug):
+    latest_articles = Article.objects.filter(status=True).order_by('-created')[:4]
+    categories = Category.objects.all()
+    category = get_object_or_404(Category,slug=slug)
+    articles = Article.objects.filter(status=True,category=category)
+    context ={
+        'category':category,
+        'articles':articles,
+        'latest_articles':latest_articles,
+        'categories':categories,
+    }
+    return render(request,'blog/category_article.html',context)
     
